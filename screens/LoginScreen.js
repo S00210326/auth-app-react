7dc1a6f621bb3749af06b88d282273e05b56ce27
Paul Mc Gonigle <s00210326@atu.ie>
2023-08-09 09:42:57 +0200

    added credential error handling

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,31 @@
 import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { useState } from "react";
 
+import { login } from "../util/auth";
+import { Alert } from "react-native";
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function loginHandler({ email, password }) {
+    setIsAuthenticating(true);
+    try {
+      await login(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed!",
+        "Could not log you in, please check your credentials."
+      );
+    }
+
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Logging you in..." />;
+  }
+
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
